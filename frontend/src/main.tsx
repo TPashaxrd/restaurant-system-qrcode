15969b9866b0a4796b6c5d7d74d39e9d9d7a576b
TPashaxrd <tpashaaonline@gmail.com>
2025-03-15 09:25:21 +0300

    upload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,6 @@ import Verified from './Pages/Verification.tsx';
 import CartPage from './Pages/CartPage.tsx';
 import Laws from './Pages/Laws.tsx';
 import User from './Pages/ShowDataPage.tsx';
-import Report from './Pages/Report.tsx';
 import ResInfo from './Pages/ResInfo.tsx';
 import AdminPage from './Pages/Admin/index.tsx';
 
@@ -20,7 +19,6 @@ createRoot(document.getElementById('root')!).render(
       <Route path="/cart" element={<CartPage/>}/>
       <Route path="/laws" element={<Laws/>}/>
       <Route path='/user' element={<User/>}/>
-      <Route path='/message' element={<Report/>}/>
       <Route path="/information" element={<ResInfo/>}/>
       <Route path="*" element={<NoPage />} />
     </Routes>
